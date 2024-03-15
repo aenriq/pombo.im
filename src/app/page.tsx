42ef27e7bd688c8275cd0cec178ed8f1c25863c6
aenriq <aenriq29@asu.edu>
2024-03-14 21:31:59 -0700

    Update global CSS and add sign-in functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
-import { auth, provider } from "@/lib/firebase"; // Update the import path
+import { auth, provider, db } from "@/lib/firebase";
 import { signInWithPopup, onAuthStateChanged, User } from "firebase/auth";
-import { Textarea } from "@/components/ui/textarea";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 export default function Home() {
 	const [user, setUser] = useState<User | null>(null);
 
 	useEffect(() => {
 		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-			console.log(currentUser);
 			setUser(currentUser);
 		});
 		return () => unsubscribe();
@@ -18,23 +17,35 @@ export default function Home() {
 
 	const signInWithGoogle = async () => {
 		try {
-			await signInWithPopup(auth, provider);
+			const result = await signInWithPopup(auth, provider);
+			const user = result.user;
+
+			const userRef = doc(db, "users", user.uid);
+			const docSnap = await getDoc(userRef);
+			if (!docSnap.exists()) {
+				await setDoc(userRef, {
+					uid: user.uid,
+					email: user.email,
+					displayName: user.displayName,
+					photoURL: user.photoURL,
+				});
+			}
 		} catch (error) {
 			console.error(error);
 		}
 	};
 
 	return (
-		<div className="main-grid">
-			<div className="grid w-full gap-2">
-				<Textarea placeholder="Type your message here." />
-				<Button>Send message</Button>
+		<>
+			<div className="flex flex-col items-center justify-center">
+				{user ? (
+					<Button className="w-max">Begin Writing</Button>
+				) : (
+					<Button className="w-max" onClick={signInWithGoogle}>
+						Sign In
+					</Button>
+				)}
 			</div>
-			{user ? (
-				<Button>Begin Writing</Button>
-			) : (
-				<Button onClick={signInWithGoogle}>Sign In</Button>
-			)}
-		</div>
+		</>
 	);
 }
