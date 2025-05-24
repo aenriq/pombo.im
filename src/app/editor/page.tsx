f4c0cabbcd3a60e739b6208e686444c3a35819a7
aenriq <aenriq29@asu.edu>
2025-05-23 22:51:52 -0700

    fresh start

--- src/app/editor/page.tsx
+++
@@ -1,6 +0,0 @@
-import { MilkdownEditorWrapper } from "@/components/editor/Editor"
-
-export default function Page() {
-	return <MilkdownEditorWrapper />
-	// return <div>hi</div>
-}
--- src/app/page.tsx
+++
@@ -1,4 +0,0 @@
-
-export default function Page() {
-	return <h1 className="col-start-2 text-3xl font-bold underline ">Hello, Next.js!</h1>
-}
